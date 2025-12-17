6ea3fe7bd7ee640726d519169c8f78d0973327f1
Daniel Robles López <danrolodev@gmail.com>
2025-12-17 15:59:54 -0600

    Initial commit

---
+++ danroloApexPredators/danroloApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  danroloApexPredatorsApp.swift
+//  danroloApexPredators
+//
+//  Created by daniel.a.robles on 16/12/25.
+//
+
+import SwiftUI
+
+@main
+struct danroloApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
